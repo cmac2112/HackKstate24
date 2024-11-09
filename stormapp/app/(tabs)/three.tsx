80eb069f08ad846e258122c27b6857d2ed2ba791
MicahQu <142445353+MicahQu@users.noreply.github.com>
2024-11-09 11:15:01 -0600

    working on get location

--- stormapp/app/(tabs)/three.tsx
+++ stormapp/app/(tabs)/three.tsx
@@ -2,32 +2,51 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import EditScreenInfo from '@/components/EditScreenInfo';
+import GetLocation from 'react-native-get-location'
 
 const three = () => {
-  return (
-    <View style={styles.container}>
-    
-      <MapView
-        style={styles.map}
-        initialRegion={{
+  let lat = 0;
+  let lon= 0;
+
+  GetLocation.getCurrentPosition({
+    enableHighAccuracy: true,
+    timeout: 60000,
+  })
+    .then(location => {
+      console.log(location);
+      lat = location.latitude;
+      lon = location.longitude;
+    })
+  .catch(error => {
+    const { code, message } = error;
+    console.warn(code, message);
+})
+
+return (
+  <View style={styles.container}>
+  
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: lat, // Replace with your latitude
+        longitude: lon, // Replace with your longitude
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+      <Marker
+        coordinate={{
           latitude: 37.78825, // Replace with your latitude
           longitude: -122.4324, // Replace with your longitude
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
         }}
-      >
-        <Marker
-          coordinate={{
-            latitude: 37.78825, // Replace with your latitude
-            longitude: -122.4324, // Replace with your longitude
-          }}
-          title={"My Location"}
-          description={"This is where I am"}
-        />
-      </MapView>
-      <EditScreenInfo path="app/(tabs)/three.tsx" />
-    </View>
-  );
+        title={"My Location"}
+        description={"This is where I am"}
+      />
+    </MapView>
+    <EditScreenInfo path="app/(tabs)/three.tsx" />
+  </View>
+);
+
 };
 
 const styles = StyleSheet.create({
